a857b23e4f4fdc236f0e885561a4af86f8596b18
jeremy-b <jeremy.steven.bess@gmail.com>
2022-08-10 17:31:28 -0700

    Add event listenerst to input buttons, display value on click

--- scripts.js
+++ scripts.js
@@ -18,3 +18,14 @@ function operate(operator, num1, num2){
     let value = operator(num1, num2)
     return value
 }
+
+let display = document.getElementById('display')
+let inputButtons = document.getElementsByClassName('input')
+
+for (let i = 0; i < inputButtons.length; i++){
+    inputButtons[i].addEventListener('click', function (e) {
+    number = e.target.getAttribute('id')
+    display.textContent += `${number}`
+})
+}
+
