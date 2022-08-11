97856fedb8315eab1a766e70ead87f4033e3ce1c
jeremy-b <jeremy.steven.bess@gmail.com>
2022-08-10 18:51:23 -0700

    Add clear button functionality, limit decimal input to one

--- scripts.js
+++ scripts.js
@@ -19,13 +19,29 @@ function operate(operator, num1, num2){
     return value
 }
 
-let display = document.getElementById('display')
-let inputButtons = document.getElementsByClassName('input')
+let display = document.getElementById('display');
+let inputButtons = document.getElementsByClassName('input');
+let storedNum = display.textContent;
 
 for (let i = 0; i < inputButtons.length; i++){
     inputButtons[i].addEventListener('click', function (e) {
     number = e.target.getAttribute('id')
+    if (display.textContent.includes('.') && number == '.'){
+        return
+    } else {
     display.textContent += `${number}`
+    }
 })
 }
 
+const plusButton = document.getElementById('plus');
+const minusButton = document.getElementById('minus');
+const divideButton = document.getElementById('divide');
+const multiplyButton = document.getElementById('times');
+
+const clearButton = document.getElementById('clear')
+clearButton.addEventListener('click', clearDisplay)
+
+function clearDisplay(){
+    display.textContent = '';
+}
